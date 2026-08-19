4a912af4d53beb7355ba7edb8bcf2ee558592510
samitkumarpatel <samitkumarpatel@gmail.com>
2026-08-20 01:01:49 +0200

    mailjet dns varifier added

--- infrastructure/azure/environments/dev/main.tf
+++ infrastructure/azure/environments/dev/main.tf
@@ -67,22 +67,23 @@ module "dns_bootstrapping" {
       values = [
         "v=spf1 include:spf.mailjet.com include:zohomail.eu ~all",
         "zoho-verification=zb86027192.zmverify.zoho.eu",
+        "zoho-verification=zb49352062.zmverify.zoho.eu",
       ]
     }
     zoho_dkim = {
       type   = "TXT"
       name   = "zmail._domainkey"
-      values = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCW91cv/+AW+BouuQNOHlzvJGYSrBTLftd9D2shUP/79LOO0cDOwhSNCvN+/nrW09TYUIfbpLF4DAp9pOavHyS95NnxPnq6i1z1ghcdqcQy5KlGkBI3tCOEgqF2yKlN69jBCic6W7NVO70hWrTJ9LYI2CWpz3bAXwiiAEVGpnNRrQIDAQAB"]
+      values = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDfUTuWe6hESQXEIDuDRv10hluIbyiauYvAHfeS40Gd7/HXru9XQGhMqOGvCb4Phsd41y4eqcwMpWefyV7EzC4Sf385U5IH8mBkFS0960tZGihTJjranMFUCEElsV0ROD08L0R9IJ1FhPWENMPshPn+49HXjOn3rUKuFEDaNYa/BQIDAQAB"]
     }
     mailjet_dkim = {
       type   = "TXT"
       name   = "mailjet._domainkey"
-      values = ["v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3H50B8nSmiEZ1l/Rzdj/MIg0eol1S1d7NnD6gGrb3QOq0gmw8nt5kBjf/qWCK3QCufnkMFDhph5gf8G+IsLPI+v6yCqG0A7JtQVmsptnTzLwR7LxDv6SoLpWQ7WeblUVNu+38JMorWaWcSNWtSt7bhJ5k9nsLvWeoo+wx+kchZ7n8vGfoQxo2NyV+pRCakqX6SX5KHnic5sw5RPKUM8PZeDYuoobShWkeSSmZIb98+Poe4wj0gjX6+IX96pj8d7HUJ8qjkvXVaEZGV6xIgV0JoJ73OM0MQedApCFCdO1dlDJmFUpWKuddwPaI6z/x/Ta2B46otZ4EEcvho9QLPyxEwIDAQAB"]
+      values = ["k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk0ZlEpvpwmCVOqwhorC9E91b7T7OP7w7dyCN+8XA+6AIXVgDS8qRAqGN319A1vgCV7nL5O1LyorI3Zii1SIwrenYUM99P3PVoVdDBnHqifrx65kEAuIppPc1K5m5w7nr4FHMCFkjmgUtI5gW0HPrL7fKF36/38/GuhgVnjUZEJHYonlacXHN3MnyDKLd063sZGkiNEYKPwdAa9Cf+nvJNj4HUI6CbSfRoY9oZdYx3wBJkVtSC+5oEag289xuxZuGJ0/MSrUfF2IIbXXewtDGMD2vfc4gWWivlSAiEm9BwzgUzVAsO9kdCJAKDQRkVW2ReKciPu8dw7+EJ3QU8n7rkQIDAQAB"]
     }
-    mailjet_verify = {
+    mailjet_verify2 = {
       type   = "TXT"
-      name   = "mailjet._8bc30511"
-      values = ["8bc30511bed47420ccc221dca77c12b8"]
+      name   = "mailjet._4ae89c73"
+      values = ["4ae89c73572f378b6006daff9627062e"]
     }
     # CDN CNAME records — added after first frontend apply exposes CDN hostnames
     # onboarding = { type = "CNAME", name = "@",           values = ["<onboarding-cdn-hostname>"] }
